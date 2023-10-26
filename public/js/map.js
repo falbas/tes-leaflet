4665bb3e92d207c149895865231e26f109a3d217
falbas <farhanalbas30@gmail.com>
2023-10-26 09:08:12 +0700

    fix/tile map service

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,10 @@
-var bounds = [
+const bounds = [
   [-11, 90],
   [11, 145],
 ]
+const map_be_url = 'http://localhost:3300/api/storage'
 
-var map = L.map('map', {
+const map = L.map('map', {
   center: [-6.17396, 106.8271],
   zoom: 6,
   minZoom: 6,
@@ -13,58 +14,40 @@ var map = L.map('map', {
 
 map.fitBounds(bounds)
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
-  maxZoom: 15,
+L.tileLayer(`${map_be_url}/20230901/tiles/temp/{z}/{x}/{y}.png`, {
+  maxZoom: 10,
   attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map)
 
-// L.tileLayer('http://127.0.0.1:5500/basemap/{z}/{x}/{y}.png', {
-//   maxZoom: 15,
-//   attribution:
-//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
-// }).addTo(map)
+const url_json = '/countries.geo.json'
+const json = fetch(url_json).then((r) => r.text())
+Promise.resolve(json).then((v) => {
+  const country = JSON.parse(v)
+  L.geoJson(country, {
+    style: function (feature) {
+      return {
+        color: 'black',
+        weight: 1,
+        fillOpacity: 0,
+      }
+    },
+  }).addTo(map)
+})
 
-// L.tileLayer
-//   .wms('http://34.16.103.1/ncWMS2/wms', {
-//     layers: 'Test/t2m',
-//     styles: 'raster/seq-Oranges',
-//     maxZoom: 19,
-//     attribution:
-//       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
-//   })
-//   .addTo(map)
-
-// var url_json =
-//   'https://openlayers.org/en/v4.6.5/examples/data/geojson/countries.geojson'
-// var json = fetch(url_json).then((r) => r.text())
-// Promise.resolve(json).then((v) => {
-//   var country = JSON.parse(v)
-//   L.geoJson(country).addTo(map)
-// })
-
-var url_u = '/U.asc'
-var url_v = '/V.asc'
-var url_t = '/T.asc'
-var urls = [url_u, url_v, url_t]
-var promises = urls.map((url) => fetch(url).then((r) => r.text()))
+const url_u = `${map_be_url}/20230901/U.asc`
+const url_v = `${map_be_url}/20230901/V.asc`
+const urls = [url_u, url_v]
+const promises = urls.map((url) => fetch(url).then((r) => r.text()))
 Promise.all(promises).then(function (arrays) {
-  //wind
-  let vf = L.VectorField.fromASCIIGrids(arrays[0], arrays[1], 50)
-  let windLayer = L.canvasLayer.vectorFieldAnim(vf)
-
-  //temp
-  var sf = L.ScalarField.fromASCIIGrid(arrays[2])
-  let tempLayer = L.canvasLayer.scalarField(sf, {
-    color: chroma.scale('OrRd').domain(sf.range),
-  })
+  const vf = L.VectorField.fromASCIIGrids(arrays[0], arrays[1], 50)
+  const windLayer = L.canvasLayer.vectorFieldAnim(vf)
 
   L.control
     .layers(
       {},
       {
         Wind: windLayer,
-        Temperature: tempLayer,
       }
     )
     .addTo(map)
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
 
       <Script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js" />
       <Script src="//d3js.org/d3.v4.min.js" />
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.4.2/chroma.min.js" />
       <Script src="/js/leaflet.canvaslayer.field.js" />
       <Script src="/js/map.js" />
     </>
