f7a6e303dd1e8a6bc52aa1296b34fc88564f7576
falbas <farhanalbas30@gmail.com>
2023-10-27 21:54:04 +0700

    add/map player

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,16 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <>
-      <div id="map"></div>
+      <div id="map" className="relative z-0"></div>
+      <div className="absolute bottom-0 z-10 flex w-screen">
+        <button id="btnPrev" className="bg-slate-400 p-4 rounded ml-1 flex-none">
+          {'<'}
+        </button>
+        <button id="btnNext" className="bg-slate-400 p-4 rounded ml-1 flex-none">
+          {'>'}
+        </button>
+        <input id="dateRange" type="range" className="grow mx-2" />
+      </div>
 
       <Script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js" />
       <Script src="//d3js.org/d3.v4.min.js" />
